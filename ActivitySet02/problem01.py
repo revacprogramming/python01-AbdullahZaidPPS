675fc66dea07d61a0eb0de399269731bc427a452
Abdullah M Zaid <95867207+AbdullahZaidPPS@users.noreply.github.com>
2022-05-18 10:41:32 +0000

    18/5

--- ActivitySet02/problem01.py
+++ ActivitySet02/problem01.py
@@ -1,12 +1,14 @@
 
-
+def in_put():
+  a=float(input("Enter number"))
+  return a
 def add(a, b):
-    return  # ...
-
-
+  z=x+y
+  return z
+def output(a,b,z):
+  print("The sum is of {} and {} is {}".format(a,b,j))
 def main():
-    a = input  # ...
-    b = input  # ...
-
-    c = add(a, b)
-    print  # ...
+  c=input()
+  d=input()
+  e=add(a,b)
+  output(c,d,e)
