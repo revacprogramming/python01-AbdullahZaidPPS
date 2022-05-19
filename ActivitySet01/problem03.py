2eef21ce07f7a999510e176901ef14c4de73f229
Abdullah M Zaid <95867207+AbdullahZaidPPS@users.noreply.github.com>
2022-05-19 00:38:57 +0000

    19/5

--- ActivitySet01/problem03.py
+++ ActivitySet01/problem03.py
@@ -1,3 +1,9 @@
 # Variables, Expressions & Statements
-
+  
+def res(hrs,rt):
+  salary = hrs*rt
+  return salary
 hrs = float(input("Enter hours? "))
+rt = float(input("Enter hourly rate: "))
+s=res(hrs,rt)
+print("The salary is ",s)
